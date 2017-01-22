311e2f38a5a7098dbdc0e5527307e1cc3e48d2e8
Łukasz Korol <lukas.korol@gmail.com>
2017-01-22 20:38:32 +0100

    Setup homepage

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe "navigate" do
   describe "homepage" do
-    visit root_path
-    expect(page).to have_http_status(200)
+    it "can be reached successfully" do
+      visit root_path
+      expect(page).to have_http_status(200)
+    end
   end
 end
